fcdb9d3750032afc37ff4abb1b4baed25b1885f4
btran <btran@Admins-MacBook-Pro-2.local>
2020-03-18 12:06:26 -0700

    Moved config into src folder

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
-const  path = require("path");
+import  path from "path";
 const { NODE_ENV } = process.env;
 let keys = "";
 
 if (NODE_ENV === "dev") {
   keys = require("dotenv").config({
-    path: path.join(__dirname, "./.env.dev")
+    path: path.join(__dirname, "../.env.dev")
   });
 }
 else if (NODE_ENV === "production") {
   keys = require("dotenv").config({
-    path: path.join(__dirname, "./.env.production")
+    path: path.join(__dirname, "../.env.production")
   });
 }
 
