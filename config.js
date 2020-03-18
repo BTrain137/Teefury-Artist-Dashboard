57b9a81a5f036664d7f58cfda51622887889a468
btran <btran@Admins-MacBook-Pro-2.local>
2020-03-18 09:30:00 -0700

    Config file for all environment

---
+++ config.js
@@ -0,0 +1,16 @@
+const  path = require("path");
+const { NODE_ENV } = process.env;
+let keys = "";
+
+if (NODE_ENV === "dev") {
+  keys = require("dotenv").config({
+    path: path.join(__dirname, "./.env.dev")
+  });
+}
+else if (NODE_ENV === "production") {
+  keys = require("dotenv").config({
+    path: path.join(__dirname, "./.env.production")
+  });
+}
+
+module.exports = keys;
