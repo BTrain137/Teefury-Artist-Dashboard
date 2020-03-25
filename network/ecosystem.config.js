f251b86dd98dbcd62b29b920f8d47ec405a2e58b
btran <btran@Admins-MacBook-Pro-2.local>
2020-03-25 12:39:08 -0700

    Add restart max memory memory into configuration

--- network/ecosystem.config.js
+++ network/ecosystem.config.js
@@ -1,10 +1,14 @@
 // pm2 start ./network/ecosystem.config.js --env production
+// pm2 start ./network/ecosystem.config.js --env dev
+// ll /root/.pm2/logs/
+
 module.exports = {
   apps: [
     {
       name: "artist-dashboard",
       script: "npm start",
-      env: {
+      max_memory_restart: "200M",
+      env_dev: {
         NODE_ENV: "dev"
       },
       env_production: {
