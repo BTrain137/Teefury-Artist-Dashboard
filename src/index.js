004dd0ac718c64bee6988f7d652e596081fefd6f
btran <btran@Admins-MacBook-Pro-2.local>
2020-04-03 09:10:44 -0700

    Complete installation of passport.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import cors from "cors";
 import logger from "morgan";
 import helmet from "helmet";
 import path from "path";
+import passport from "passport"
 import routes from "./routes";
 import "./database";
+import "./services/passport.js";
 
 const app = express();
 const { NODE_ENV, PORT } = process.env;
@@ -16,10 +18,31 @@ app.use(helmet());
 app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
 if (NODE_ENV === "development") app.use(logger("dev"));
 
 // API Routes
 app.use(routes);
+
+app.use((error, req, res, next) => {
+  if (NODE_ENV === "development") {
+    console.log("-----------------------------------------------");
+    console.log("Error status: ", error.status);
+    console.log("Message: ", error.message);
+    console.log("Header Sent: ", res.headersSent);
+    console.log("Stack: ", error.stack);
+    console.log("-----------------------------------------------");
+  }
+
+  if (res.headersSent) {
+    return next(error);
+  }
+
+  res.status(error.status || 500).json({
+    status: error.status,
+    message: error.message
+  });
+});
 
 // Serve React In Production
 if (NODE_ENV === "production") {
