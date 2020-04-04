d4facabb66e26a5a1ba459030c735fe7772d257b
btran <btran@Admins-MacBook-Pro-2.local>
2020-04-03 17:58:47 -0700

    Remove test side data from schema. And renamed tables

---
+++ src/database/seed.sql
@@ -0,0 +1,13 @@
+USE `artist_dashboard`;
+
+-- Test Data
+
+-- Insert Many 
+INSERT INTO `artist_profile` (`artist_name`, `first_name`, `last_name`, `username_contact_email`, `paypal_email`, `phone`, `social_facebook`, `social_instagram`, `social_twitter`, `international`) 
+VALUES ("locoMotive", "Tom", "Thomas", "tom@email.com", "tom@email.com", "", "", "", "",  true),
+("babyUprising", "John", "Johnson", "john@email.com", "john@email.com", "", "", "", "",  false),
+("jacksonFive", "Jack", "Jackson", "jack@email.com", "jack@email.com", "", "", "", "",  true),
+("B-train", "Bryan", "Tran", "btran@teefury.com", "btran@teefury.com", "", "", "", "",  false);
+
+-- Update One
+-- UPDATE `artist_profile` SET `first_name`="Henry" WHERE `artist_name`="locoMotive";
