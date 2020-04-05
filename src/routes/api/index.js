e92c412251e5a13295c07a92d6cb1d04ea4853dc
btran <btran@Admins-MacBook-Pro-2.local>
2020-04-04 19:23:22 -0700

    Add artist routes into api backend

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,10 +2,13 @@ import { Router } from "express";
 const router = Router();
 import helloWorld from './helloWorld.js';
 import passportRoutes from "./passport";
+import artistRoutes from "./artist";
 
 // Test Route
 router.use(helloWorld);
 
 router.use(passportRoutes);
+
+router.use(artistRoutes);
 
 export default router;
