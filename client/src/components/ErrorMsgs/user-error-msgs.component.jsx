c6cead2bef1aa13b2f0ffecd481d59e30ecb117e
btran <btran@Admins-MacBook-Pro-2.local>
2020-04-13 16:30:03 -0700

    Created Error message folder
    Might refactor and send all error messages into that folder

--- client/src/components/ErrorMsgs/user-error-msgs.component.jsx
+++ client/src/components/ErrorMsgs/user-error-msgs.component.jsx
@@ -2,25 +2,29 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { selectArtistError } from "../../redux/artist/artist.selector";
+import { selectUserError } from "../../redux/user/user.selector";
 
-import { ErrorTitle, ErrorList, FormTitle } from "./artist-err-msg.styles";
+import { ErrorTitle, ErrorList, FormTitle } from "./error-msgs.styles";
 
 /**
  * @param {Object}   error          Error object
  * @param {Number}   error.status   HTTP error status
  * @param {String[]} error.messages Error message in an array
  */
-const ArtistErrorMessages = ({ error, children }) => {
+const UserError = ({ error, children }) => {
   if (error) {
     const { messages } = error;
-    console.log("ArtistErrorMessages", { messages });
+    console.log("UserError: ", { messages });
     return (
       <ErrorList>
         <ErrorTitle>Please Correct Error(s)</ErrorTitle>
-        {messages.map((errMsg, i) => (
-          <li key={i} dangerouslySetInnerHTML={{ __html: errMsg }} />
-        ))}
+        {messages.length > 0 ? (
+          messages.map((errMsg, i) => (
+            <li key={i} dangerouslySetInnerHTML={{ __html: errMsg }} />
+          ))
+        ) : (
+          <li>Sorry Something Went Wrong.</li>
+        )}
       </ErrorList>
     );
   } else {
@@ -37,7 +41,7 @@ const ArtistErrorMessages = ({ error, children }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-  error: selectArtistError,
+  error: selectUserError,
 });
 
-export default connect(mapStateToProps)(ArtistErrorMessages);
+export const UserErrorMessages = connect(mapStateToProps)(UserError);
