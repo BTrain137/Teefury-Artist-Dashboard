6b3ebdd0aee83725879e0338b1c8996d33d03c74
btran <btran@Admins-MacBook-Pro-2.local>
2020-04-14 20:50:44 -0700

    Removed hello world api test routes

--- api/src/routes/api/helloWorld.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-const router = Router();
-
-// /api/hello-world
-router.get("/hello-world", (_, res) => {
-  res.json({ Hello: "world" });
-});
-
-export default router;
--- api/src/routes/api/index.js
+++ api/src/routes/api/index.js
@@ -1,11 +1,7 @@
 import { Router } from "express";
 const router = Router();
-import helloWorld from './helloWorld.js';
 import userRoutes from "./user";
 import artistRoutes from "./artist";
-
-// Test Route
-router.use(helloWorld);
 
 router.use(userRoutes);
 
