c510132ad449343186e94a69ed89465b1fb7c3d4
btran <btran@Admins-MacBook-Pro-2.local>
2020-04-16 19:04:28 -0700

    Add shopify's clean string used for directory

--- api/src/utils/cleanData.js
+++ api/src/utils/cleanData.js
@@ -1,3 +1,58 @@
+
+/**
+ * Clean title or vendor string to search in shopify
+ * 
+ * @param  {String} str  Any string to be cleaned
+ * @return {String}      The same string minus special characters 
+ */
+
+export const cleanStringShopify = function(str) {
+  let tempStr = str
+      // Remove all special characters
+      .replace(/\*\*/gi, "-")
+      .replace(/!/gi, "")
+      .replace(/\[/g, "")
+      .replace(/\]/g, "")
+      .replace(/'/g, "")
+      .replace(/"/g, "")
+      .replace(/\#/g, "")
+      .replace(/\$/g, "")
+      .replace(/\%/g, "")
+      .replace(/\&/g, "")
+      .replace(/\?/g, "")
+      .replace(/\)/g, "")
+      .replace(/\(/g, "")
+      .replace(/\+/g, "")
+      .replace(/\//g, "")
+      .replace(/\,/g, "")
+      .replace(/\~/g, "")
+      .replace(/\{/g, "")
+      .replace(/\}/g, "")
+      .replace(/\|/g, "")
+      .replace(/\}/g, "")
+      .replace(/\\/g, "")
+      .replace(/\:/g, "")
+      .replace(/\*/g, "-")
+      .replace(/\@/g, "-")
+      // Remove all double spaces
+      .replace(/\.\.\./g, "")
+      .replace(/  /g, " ")
+      // Replace some characters with dashes instead
+      .replace(/ /g, "-")
+      .replace(/\./g, "-")
+      .replace(/\^/g, "-")
+
+      // replace one or more hyphens with a single hyphen, globally
+      .replace(/-+/g,"-") 
+
+      // replace all hyphens at beginning and end of string.
+      .replace(/^-+|-+$/g,"") 
+
+      .toLowerCase();
+  
+  return tempStr;
+};
+
 /**
  * @description Remove some special characters, replace space to underscore and make everything lowercase
  * @param  {String} str Input string
