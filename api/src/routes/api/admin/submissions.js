e1e368efd43e524abff915aaf02a9cbdf8fc8ff1
btran <btran@Admins-MacBook-Pro-2.local>
2020-04-24 13:54:35 -0700

    Create get one submission for admins

--- api/src/routes/api/admin/submissions.js
+++ api/src/routes/api/admin/submissions.js
@@ -20,7 +20,9 @@ router.post(
         "`art_file` AS `artFile`, `preview_art` AS `previewArt`, `status`, " +
         "`created_at` AS `createdAt` FROM `submissions` " +
         "ORDER BY `created_at` DESC " +
-        "LIMIT " + startAt + ",100 "; 
+        "LIMIT " +
+        startAt +
+        ",100 ";
 
       /**
        * @return {SubmissionDetails[]}
@@ -29,6 +31,37 @@ router.post(
       conn.end();
 
       res.status(200).json({ submissionsDetailsArr });
+    } catch (error) {
+      conn.end();
+      next(error);
+    }
+  }
+);
+
+router.get(
+  "/submissions/review/:id",
+  // passport.authenticate("jwt-admin"),
+  async (req, res, next) => {
+    const { id: submissionId } = req.params;
+    let conn;
+
+    try {
+      conn = await pool.getConnection();
+      const queryString =
+        "SELECT `id`, `artist_name` AS `artistName`, `title`, `description`, " +
+        "`art_file` AS `artFile`, `preview_art` AS `previewArt`, `status`, " +
+        "`created_at` AS `createdAt` FROM `submissions` " +
+        "WHERE `id`=?";
+      /**
+       * @return {SubmissionDetails}
+       */
+      const [submissionDetails] = await pool.query(queryString, [
+        submissionId,
+      ]);
+
+      conn.end();
+
+      res.status(200).json({ submissionDetails });
     } catch (error) {
       conn.end();
       next(error);
