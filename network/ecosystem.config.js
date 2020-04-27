864c915ac59c337e62a9e53f998ca12d34c50238
btran <btran@Admins-MacBook-Pro-2.local>
2020-04-27 08:57:53 -0700

    Setup fresh project to deploy

--- network/ecosystem.config.js
+++ network/ecosystem.config.js
@@ -6,14 +6,25 @@ module.exports = {
   apps: [
     {
       name: "artist-dashboard",
+      cwd: "/var/www/artist-dashboard/source",
       script: "npm start",
       max_memory_restart: "200M",
       env_dev: {
-        NODE_ENV: "development"
+        NODE_ENV: "development",
       },
       env_production: {
-        NODE_ENV: "production"
-      }
-    }
-  ]
+        NODE_ENV: "production",
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      user: "root",
+      host: ["95.217.162.146"],
+      path: "/var/www/artist-dashboard",
+      repo: "https://github.com/btran-teefury/artist-dashboard-2.git",
+      "post-deploy":
+        "npm install && pm2 startOrRestart ./network/ecosystem.config.js --env production",
+    },
+  },
 };
