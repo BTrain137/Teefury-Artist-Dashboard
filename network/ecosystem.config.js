9acabda0c5db5c147b8a508be64b3ff8f17e76ee
btran <btran@Admins-MacBook-Pro-2.local>
2020-04-27 09:20:14 -0700

    Corrected pm2 settings file set up npm script to deploy project

--- network/ecosystem.config.js
+++ network/ecosystem.config.js
@@ -1,3 +1,4 @@
+// pm2 deploy production --force
 // pm2 start ./network/ecosystem.config.js --env production
 // pm2 start ./network/ecosystem.config.js --env dev
 // ll /root/.pm2/logs/
@@ -20,11 +21,13 @@ module.exports = {
   deploy: {
     production: {
       user: "root",
+      key: "~/.ssh/id_rsa_hetnzer_artist_dashboard_2",
       host: ["95.217.162.146"],
+      ref: "origin/master",
       path: "/var/www/artist-dashboard",
       repo: "https://github.com/btran-teefury/artist-dashboard-2.git",
       "post-deploy":
-        "npm install && pm2 startOrRestart ./network/ecosystem.config.js --env production",
+        "npm install && cd client && npm install && cd ./.. && pm2 startOrRestart ./network/ecosystem.config.js --env production",
     },
   },
 };
