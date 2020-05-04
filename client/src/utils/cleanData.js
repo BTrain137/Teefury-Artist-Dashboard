90409c42f4d2c0f20d5602a1422ba1c6d476ebd2
btran <btran@Admins-MacBook-Pro-2.local>
2020-05-04 15:10:23 -0700

    Convert time from mariadb to localtime

--- client/src/utils/cleanData.js
+++ client/src/utils/cleanData.js
@@ -12,3 +12,20 @@ export const cleanFileName = (str) => {
     .replace(/ /g, "_")
     .toLowerCase();
 };
+
+/**
+ * @description Convert Mariadb time into English readable time
+ * @param  {String} str Time from mariadb
+ * @return {String} Cleaned up string
+ * @example
+ * convertTime("2020-05-01T00:07:20.000Z")
+ * // '5/1/2020, 12:07:00 AM'
+ */
+
+export const convertTime = (time) => {
+  const spiltTime = time.split(":");
+  spiltTime.pop();
+  const timeString = spiltTime.join(":");
+  const americaTime = new Date(timeString).toLocaleString("en-US");
+  return americaTime;
+}
