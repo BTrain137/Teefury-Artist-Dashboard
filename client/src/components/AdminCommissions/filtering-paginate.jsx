5ce2d2f6744126cfd5c04813adc4422a016b9059
btran <btran@Admins-MacBook-Pro-2.local>
2020-05-04 17:44:53 -0700

    Both filtering and pagination combined

---
+++ client/src/components/AdminCommissions/filtering-paginate.jsx
@@ -0,0 +1,349 @@
+import React, { useMemo, useEffect, useState } from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import styled from "styled-components";
+import { useTable, useFilters, usePagination } from "react-table";
+import matchSorter from "match-sorter";
+
+import { convertTime } from "../../utils/cleanData";
+import { selectUserJWTToken } from "../../redux/user/user.selector";
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+    width: 100%;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+
+  .pagination {
+    padding: 0.5rem;
+    text-align: center;
+    margin-top: 25px;
+  }
+`;
+
+// Define a default UI for filtering
+function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length;
+
+  return (
+    <input
+      value={filterValue || ""}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  );
+}
+
+// This is a custom filter UI for selecting
+// a unique option from a list
+function SelectColumnFilter({
+  column: { filterValue, setFilter, preFilteredRows, id },
+}) {
+  // Calculate the options for filtering
+  // using the preFilteredRows
+  const options = React.useMemo(() => {
+    const options = new Set();
+    preFilteredRows.forEach((row) => {
+      options.add(row.values[id]);
+    });
+    return [...options.values()];
+  }, [id, preFilteredRows]);
+
+  // Render a multi-select box
+  return (
+    <select
+      value={filterValue}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined);
+      }}
+    >
+      <option value="">All</option>
+      {options.map((option, i) => (
+        <option key={i} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+function fuzzyTextFilterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, { keys: [(row) => row.values[id]] });
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = (val) => !val;
+
+// Our table component
+function Table({ columns, data }) {
+  const filterTypes = React.useMemo(
+    () => ({
+      // Add a new fuzzyTextFilterFn filter type.
+      fuzzyText: fuzzyTextFilterFn,
+      // Or, override the default text filter to use
+      // "startWith"
+      text: (rows, id, filterValue) => {
+        return rows.filter((row) => {
+          const rowValue = row.values[id];
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true;
+        });
+      },
+    }),
+    []
+  );
+
+  const defaultColumn = useMemo(() => ({ Filter: DefaultColumnFilter }), []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+
+    // Paginate
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize, filters },
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn, 
+      filterTypes,
+    },
+    useFilters, 
+    usePagination
+  );
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup, i) => (
+            <tr key={i} {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column, j) => (
+                <th key={j} {...column.getHeaderProps()}>
+                  {column.render("Header")}
+                  {/* Render the columns filter UI */}
+                  <div>{column.canFilter ? column.render("Filter") : null}</div>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr key={i} {...row.getRowProps()}>
+                {row.cells.map((cell, j) => {
+                  return (
+                    <td key={j} {...cell.getCellProps()}>
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <br />
+
+      {/* Pagination */}
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>{" "}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {"<"}
+        </button>{" "}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {">"}
+        </button>{" "}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>{" "}
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <span>
+          | Go to page:{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page);
+            }}
+            style={{ width: "100px" }}
+          />
+        </span>{" "}
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {process.env.NODE_ENV === "development" ? (
+        <div>
+          <pre>
+            <code>
+              {JSON.stringify(
+                {
+                  filters,
+                  pageIndex,
+                  pageSize,
+                  pageCount,
+                  canNextPage,
+                  canPreviousPage,
+                },
+                null,
+                2
+              )}
+            </code>
+          </pre>
+        </div>
+      ) : null}
+    </>
+  );
+}
+
+function App({ token }) {
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Date",
+        accessor: "order_created_at",
+        disableFilters: true,
+      },
+      {
+        Header: "Order #",
+        accessor: "order",
+        filter: "fuzzyText",
+      },
+      {
+        Header: "Title",
+        accessor: "product_title",
+        filter: "fuzzyText",
+      },
+      {
+        Header: "Vendor",
+        accessor: "vendor",
+        filter: "fuzzyText",
+      },
+      {
+        Header: "Product Type",
+        accessor: "product_type",
+        Filter: SelectColumnFilter,
+        filter: "equals",
+      },
+      {
+        Header: "Commissions Amount",
+        accessor: "commissions_amount",
+        disableFilters: true,
+      },
+      {
+        Header: "Paid or Unpaid",
+        accessor: "commissions_paid",
+        Filter: SelectColumnFilter,
+        filter: "includes",
+      },
+    ],
+    []
+  );
+
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const getAllCommissions = async () => {
+      const reqBody = {
+        startAt: 1,
+      };
+      const {
+        data: { commissionsDetailsArr },
+      } = await axios.post("/api/admin/commissions", reqBody, {
+        headers: { Authorization: `JWT ${token}` },
+      });
+
+      const convertDetailsForAdmin = commissionsDetailsArr.map((details) => {
+        const {
+          order_created_at,
+          commissions_paid,
+          ...otherProperty
+        } = details;
+        return {
+          ...otherProperty,
+          order_created_at: convertTime(order_created_at),
+          commissions_paid: commissions_paid ? "Paid" : "Unpaid",
+        };
+      });
+
+      setTableData(convertDetailsForAdmin);
+    };
+
+    getAllCommissions();
+  }, [token]);
+
+  return (
+    <Styles>
+      <Table columns={columns} data={tableData} />
+    </Styles>
+  );
+}
+
+const mapStateToProps = createStructuredSelector({
+  token: selectUserJWTToken,
+});
+
+export default connect(mapStateToProps)(App);
