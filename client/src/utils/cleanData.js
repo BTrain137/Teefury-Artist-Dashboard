36255a4566183718bf73249d0989c354e6eae042
btran <btran@Admins-MacBook-Pro-2.local>
2020-05-05 14:20:10 -0700

    Parse mariadb for just date

--- client/src/utils/cleanData.js
+++ client/src/utils/cleanData.js
@@ -29,3 +29,20 @@ export const convertTime = (time) => {
   const americaTime = new Date(timeString).toLocaleString("en-US");
   return americaTime;
 }
+
+/**
+ * @description Convert Mariadb time into English readable time
+ * @param  {String} str Time from mariadb
+ * @return {String} Cleaned up string
+ * @example
+ * convertTime("2020-05-01T00:07:20.000Z")
+ * // '5/1/2020,'
+ */
+
+export const justDate = (time) => {
+  const spiltTime = time.split(":");
+  spiltTime.pop();
+  const timeString = spiltTime.join(":");
+  const americaTime = new Date(timeString).toLocaleDateString("en-US");
+  return americaTime;
+}
