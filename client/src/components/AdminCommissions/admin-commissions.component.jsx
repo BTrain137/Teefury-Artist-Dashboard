52b511fdf37d52b3e484628fd987efc615e5b484
btran <btran@Admins-MacBook-Pro-2.local>
2020-05-06 15:17:43 -0700

    Move example code out of project and renamed files to proper name

--- client/src/components/AdminCommissions/admin-commissions.component.jsx
+++ client/src/components/AdminCommissions/admin-commissions.component.jsx
@@ -1,48 +1,20 @@
 import React, { Component } from "react";
 
-// import { ReactComponent as AdjustablesIcon } from "../../assets/adjustables.svg";
+import Table from "./table.component";
 
-// import Table from "../Table/Table.component";
-import Table from "./select-filtering-paginate";
-
-import {
-  SubmissionContainer,
-  TabArea,
-  // FilterHeader,
-  // AdjustableIconWrapper,
-  // FilterContainer,
-} from "../SharedStyle/styled.component";
+import { SubmissionContainer, TabArea } from "../SharedStyle/styled.component";
 
 class AdminCommissions extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      isShowingFilter: false,
-    };
+    this.state = {};
   }
 
-  toggleFilterArea = () => {
-    const { isShowingFilter } = this.state;
-    this.setState({ isShowingFilter: !isShowingFilter });
-  };
-
   render() {
-    // const { isShowingFilter } = this.state;
     return (
       <SubmissionContainer>
         <TabArea>
-          {/* <FilterHeader>
-            <AdjustableIconWrapper onClick={this.toggleFilterArea}>
-              <AdjustablesIcon />
-            </AdjustableIconWrapper>
-            {isShowingFilter ? (
-              <FilterContainer>
-                <div style={{ height: "100px", width: "50px" }} />
-              </FilterContainer>
-            ) : null}
-          </FilterHeader> */}
-
           <Table />
         </TabArea>
       </SubmissionContainer>
