c0f8bdff6b457d8acdddf07e865c628a56f6cf15
btran <btran@Admins-MacBook-Pro-2.local>
2020-05-06 14:55:57 -0700

    Created comissions update wroute

--- api/src/routes/api/admin/commissions.js
+++ api/src/routes/api/admin/commissions.js
@@ -29,12 +29,59 @@ router.post(
     try {
       conn = await pool.getConnection();
       const queryString =
-        "SELECT `order_created_at`, `order`, `product_title`, " +
+        "SELECT `id` as `dbRowId`, `order_created_at`, `order`, `product_title`, " +
         "`vendor`, `product_type`, `order_id` as `commissions_amount`, `commissions_paid` " +
         "FROM `orders` " +
         "LIMIT " +
         defaultStartAt +
-        ",100 ";
+        ",100";
+
+      /**
+       * @return {CommissionsDetails[]}
+       */
+      const commissionsDetailsArr = await pool.query(queryString);
+      conn.end();
+
+      res.status(200).json({ commissionsDetailsArr });
+    } catch (error) {
+      conn.end();
+      next(error);
+    }
+  }
+);
+
+router.put(
+  "/commissions/update",
+  passport.authenticate("jwt-admin"),
+  async (req, res, next) => {
+    const { dbRowIds, isPaid, startAt } = req.body;
+    const defaultStartAt = startAt ? startAt : 1;
+
+    let ids = "`id`=" + dbRowIds.shift();
+
+    if (dbRowIds.length > 0) {
+      ids += dbRowIds
+        .map((id) => {
+          return " OR `id`=" + id;
+        })
+        .join("");
+    }
+
+    let conn;
+    try {
+      conn = await pool.getConnection();
+      const queryUpdate =
+        "UPDATE `orders` SET `commissions_paid`=? WHERE (" + ids + ");";
+
+      const { affectedRows } = await pool.query(queryUpdate, [isPaid]);
+
+      const queryString =
+        "SELECT `id` as `dbRowId`, `order_created_at`, `order`, `product_title`, " +
+        "`vendor`, `product_type`, `order_id` as `commissions_amount`, `commissions_paid` " +
+        "FROM `orders` " +
+        "LIMIT " +
+        defaultStartAt +
+        ",100";
 
       /**
        * @return {CommissionsDetails[]}
