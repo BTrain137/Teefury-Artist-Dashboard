f15e6bb5461a23f5fd43c42610937085b034e586
btran <btran@Admins-MacBook-Pro-2.local>
2020-05-06 17:58:26 -0700

    Removed local styles to shared styles

--- client/src/components/ArtistCommissions/artist-commissions.styles.jsx
+++ client/src/components/ArtistCommissions/artist-commissions.styles.jsx
@@ -1,19 +1,1 @@
 import styled from "styled-components";
-
-export const SubmissionContainer = styled.div`
-  width: 90%;
-  margin: 40px auto;
-`;
-
-export const TabArea = styled.div`
-  box-shadow: 0px 7px 16px 2px rgba(0, 0, 0, 0.2);
-  border-radius: 15px;
-  padding: 55px;
-  min-height: 70vh;
-`;
-
-export const FilterHeader = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  position: relative;
-`;
