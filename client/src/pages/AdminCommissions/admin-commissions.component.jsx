586209aa4aa2d7af4c28327fee965dc43fb34bc5
btran <btran@Admins-MacBook-Pro-2.local>
2020-05-11 11:59:39 -0700

    Moved style to shared folder

--- client/src/pages/AdminCommissions/admin-commissions.component.jsx
+++ client/src/pages/AdminCommissions/admin-commissions.component.jsx
@@ -1,20 +1,30 @@
 import React from "react";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
 
 import Nav from "../../components/Nav";
 import AdminCommissions from "../../components/AdminCommissions";
 
-import {
-  ArtistContainer,
-  CommissionsWrapper,
-} from "./admin-commissions.styles";
+import { PageContainer, PageWrapper } from "../SharedStyle/style";
 
-const AdminCommissionsPage = () => (
-  <ArtistContainer>
-    <Nav />
-    <CommissionsWrapper>
-      <AdminCommissions />
-    </CommissionsWrapper>
-  </ArtistContainer>
-);
+const AdminCommissionsPage = () => {
+  const { path } = useRouteMatch();
+  return (
+    <PageContainer>
+      <Nav />
+      <Switch>
+        <Route exact path={path}> 
+          <PageWrapper>
+            <AdminCommissions />
+          </PageWrapper>
+        </Route>
+        <Route exact path={`${path}/payout`}> 
+          <PageWrapper>
+            <AdminCommissions />
+          </PageWrapper>
+        </Route>
+      </Switch>
+    </PageContainer>
+  );
+};
 
 export default AdminCommissionsPage;
