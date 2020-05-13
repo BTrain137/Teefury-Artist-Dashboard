3ea7e488dac70d65c6d6bca3d4752a94abe9a062
btran <btran@Admins-MacBook-Pro-2.local>
2020-05-12 20:14:36 -0700

    Admin if is not admin logout and reset

---
+++ client/src/components/AdminSummary/admin-summary.component.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useHistory } from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectUserAccount } from "../../redux/user/user.selector";
+import { logoutStart } from "../../redux/user/user.action";
+
+const AdminSummary = ({ userAccount, logOut }) => {
+  // TODO: admin summary of different components
+  let history = useHistory();
+
+  useEffect(
+    () => {
+      const redirectUser = () => {
+        const { isAdmin } = userAccount;
+        if (!isAdmin) {
+          logOut();
+          history.push("/");
+        }
+        return;
+      };
+
+      redirectUser();
+    },
+    // eslint-disable-next-line
+    [userAccount]
+  );
+
+  return <></>;
+};
+
+const mapStateToProps = createStructuredSelector({
+  userAccount: selectUserAccount,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  logOut: () => dispatch(logoutStart()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AdminSummary);
--- client/src/pages/AdminProfilePage/admin-profile-page.component.jsx
+++ client/src/pages/AdminProfilePage/admin-profile-page.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Nav from "../../components/Nav";
+import AdminSummary from "../../components/AdminSummary";
 
 import { PageContainer } from "../SharedStyle/style";
 
 const AdminProfilePage = () => (
   <PageContainer>
     <Nav />
+    <AdminSummary />
   </PageContainer>
 );
 
