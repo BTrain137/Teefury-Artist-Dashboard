c8c941d573763e9b90704bcffcb4106c9d3523ef
btran <btran@Admins-MacBook-Pro-2.local>
2020-05-12 19:43:53 -0700

    Ignore notification until there is a need for it

--- client/src/components/Nav/nav.component.jsx
+++ client/src/components/Nav/nav.component.jsx
@@ -4,6 +4,7 @@ import { createStructuredSelector } from "reselect";
 
 import { selectUserAccount } from "../../redux/user/user.selector";
 
+ // eslint-disable-next-line
 import { ReactComponent as BellIcon } from "../../assets/bell.svg";
 import { ReactComponent as CogIcon } from "../../assets/cog.svg";
 import { ReactComponent as ApprovalIcon } from "../../assets/approval.svg";
