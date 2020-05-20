cba788a83378baedf22cd5d3f4362418ee14f873
btran <btran@Admins-MacBook-Pro-2.local>
2020-05-20 09:16:26 -0700

    Basic production seed

---
+++ api/src/database/prodSeed.sql
@@ -0,0 +1,25 @@
+-- Insesrt Test Artist Account
+INSERT INTO `artist_profile` (`artist_name`, `first_name`, `last_name`, `username_contact_email`, `paypal_email`, `phone`, `social_facebook`, `social_instagram`, `social_twitter`, `is_international`) 
+VALUES ("locoMotive", "Tom", "Thomas", "bryan89tran@yahoo.com", "bryan89tran@yahoo.com", "", "", "", "",  true);
+
+INSERT INTO `users` (`username_contact_email`)
+VALUES ("bryan89tran@yahoo.com");
+
+-- Comissions payout
+INSERT INTO `payouts` (`product_type`, `commissions_payout`)
+VALUES ("Tee", 2.00),
+ ("Sweatshirt", 2.00),
+ ("Tank", 2.00),
+ ("Blanket", 2.00),
+ ("Towel", 2.00),
+ ("Leggings", 2.00),
+ ("Gallery", 2.00),
+ ("Pin", 0.50),
+ ("ODAD-Tee", 1.00),
+ ("ODAD-Sweatshirt", 1.00),
+ ("ODAD-Tank", 1.00),
+ ("Weekly-Tee", 1.00),
+ ("Weekly-Sweatshirt", 1.00),
+ ("Weekly-Tank", 1.00),
+ ("Grab Bag", 0.00),
+ ("none", 0.00);
