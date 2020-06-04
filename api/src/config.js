73581b1d88079d24075730f9b582902585b06c1f
btran <btran@Admins-MacBook-Pro-2.local>
2020-06-03 20:30:56 -0700

    Add stage into config

--- api/src/config.js
+++ api/src/config.js
@@ -1,15 +1,18 @@
-import  path from "path";
+import path from "path";
 const { NODE_ENV } = process.env;
 let keys = "";
 
 if (NODE_ENV === "development") {
   keys = require("dotenv").config({
-    path: path.join(__dirname, "../.env.development")
+    path: path.join(__dirname, "../.env.development"),
   });
-}
-else if (NODE_ENV === "production") {
+} else if (NODE_ENV === "stage") {
   keys = require("dotenv").config({
-    path: path.join(__dirname, "../.env.production")
+    path: path.join(__dirname, "../.env.stage"),
+  });
+} else if (NODE_ENV === "production") {
+  keys = require("dotenv").config({
+    path: path.join(__dirname, "../.env.production"),
   });
 }
 
