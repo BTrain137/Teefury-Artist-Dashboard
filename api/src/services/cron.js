c598df3721ceeb5e92f50d14a4857b726c1a6abd
btran <btran@Admins-MacBook-Pro-2.local>
2020-06-03 21:30:32 -0700

    Block stage from running cron jobs

--- api/src/services/cron.js
+++ api/src/services/cron.js
@@ -1,16 +1,20 @@
 import cron from "node-cron";
 import processOrders from "../libs/processOrdersFromShopify";
-import { cleanDate, startAndEndTime } from "../utils/cleanData";
+import { startAndEndTime } from "../utils/cleanData";
 
-// At the 15 minute mark of every hour
-cron.schedule("0 15 * * * *", async () => {
-  const now = new Date();
-  const { startDate, startHour, endDate, endHour } = startAndEndTime(now);
+const { NODE_ENV } = process.env;
 
-  const output = `${startDate} ${startHour} ${endDate} ${endHour}`;
-  try {
-    await processOrders({ startDate, startHour, endDate, endHour });
-  } catch (error) {
-    console.log(`CRON ERROR: -- ${output} :`, error);
-  }
-});
+if (NODE_ENV !== "stage") {
+  // At the 15 minute mark of every hour
+  cron.schedule("0 15 * * * *", async () => {
+    const now = new Date();
+    const { startDate, startHour, endDate, endHour } = startAndEndTime(now);
+
+    const output = `${startDate} ${startHour} ${endDate} ${endHour}`;
+    try {
+      await processOrders({ startDate, startHour, endDate, endHour });
+    } catch (error) {
+      console.log(`CRON ERROR: -- ${output} :`, error);
+    }
+  });
+}
