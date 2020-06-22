6b99b35f84981aa8f30eeb18676210fa980b1c6f
btran <btran@Admins-MacBook-Pro-2.local>
2020-06-22 16:03:45 -0700

    Add description for validation

--- client/src/utils/validationUtils.js
+++ client/src/utils/validationUtils.js
@@ -2,18 +2,59 @@ export const isPasswordStrong = (password) => {
   return password.length < 6 ? false : true;
 };
 
+/**
+ * Example:
+ * ```js
+ * isEmailValid('email@example')     // false
+ * isEmailValid('email@example.com') // true
+ * ```
+ * @description Email must include "at symbol" and "dot" to be valid 
+ * @param  {String}  email Email address 
+ * @return {Boolean}       If the email is valid or not
+ */
 export const isEmailValid = (contactEmail) => {
   const valid_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return valid_email.test(contactEmail);
 };
 
+/**
+ * @description Names are only lowercase or uppercase letters can include "underscores", "apostrophes", "hyphens"
+ * @param  {String}  name First name or last name 
+ * @return {Boolean}      If the email is valid or not
+ * @example 
+ * isNameValid("John")       // true
+ * isNameValid("Anna-Marie") // true
+ * isNameValid("O'Reilly")   // true
+ * isNameValid("O'Reilly")   // true
+ * isNameValid('O"Reilly')   // false
+ * isNameValid("example&")   // false
+ * isNameValid("ex.ample")   // false
+ */
 export const isNameValid = (name) => {
-  var valid_name = /^[a-zA-Z_\- ]+$/;
+  var valid_name = /^[a-zA-Z_\-\' ]+$/;
   return valid_name.test(name);
 };
 
 export const doesPasswordMatch = (oldString, newString) => {
   return oldString === newString ? true : false;
+};
+
+/**
+ * @description Phone number matches for numbers, can include "parenthesis", "plus" in front, US (domestic) and some international patterns are valid
+ * @param  {String}  phoneNumber Phone Number
+ * @return {Boolean}             If the phone number is valid or not
+ * @example 
+ * isPhoneNumberValid("(123) 456-7890") // true
+ * isPhoneNumberValid("123)456-7890")   // true
+ * isPhoneNumberValid("123-456-7890")   // true
+ * isPhoneNumberValid("123.456.7890")   // true
+ * isPhoneNumberValid("1234567890")     // true
+ * isPhoneNumberValid("+31636363634")   // true
+ * isPhoneNumberValid("075-63546725")   // true
+ */
+export const isPhoneNumberValid = (phoneNumber) => {
+  var regexPhoneNumber = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  return regexPhoneNumber.test(phoneNumber);
 };
 
 export const areUserFormFieldsValid = (contactEmail, password) => {
