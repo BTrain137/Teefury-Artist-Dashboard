da17e4ffcf55a3f48590dd3f3bc2072f9a3c82ee
btran <btran@Admins-MacBook-Pro-2.local>
2020-06-22 18:02:12 -0700

    Removed cron and deploy stage should be staging branch

--- network/ecosystem.config.js
+++ network/ecosystem.config.js
@@ -1,6 +1,5 @@
 // pm2 deploy production --force
 // pm2 start ./network/ecosystem.config.js --env production
-// pm2 start ./network/ecosystem.config.js --env dev
 // ll /root/.pm2/logs/
 // pm2 logs --timestamp
 
@@ -13,17 +12,6 @@ module.exports = {
       max_memory_restart: "200M",
       instances: "MAX",
       exec_mode: "cluster",
-      time: true,
-      env: {
-        NODE_ENV: "production",
-      },
-    },
-    {
-      name: "shopify-order-cron",
-      cwd: "/var/www/artist-dashboard/source/api/dist/services",
-      script: "cron.js",
-      max_memory_restart: "100M",
-      instances: 1,
       time: true,
       env: {
         NODE_ENV: "production",
@@ -65,14 +53,13 @@ module.exports = {
       path: "/var/www/artist-dashboard",
       repo: "https://github.com/btran-teefury/artist-dashboard-2.git",
       "post-deploy":
-        // "npm install && npm run build && cd client && npm install && npm run build && cd ./.. && pm2 startOrRestart ./network/ecosystem.config.js --only artist-dashboard && pm2 startOrRestart ./network/ecosystem.config.js --only shopify-order-cron",
         "npm install && npm run build && cd client && npm install && npm run build && cd ./.. && pm2 startOrRestart ./network/ecosystem.config.js --only artist-dashboard",
     },
     stage: {
       user: "root",
       key: "~/.ssh/id_rsa_hetnzer_artist_dashboard_2",
       host: ["95.217.162.146"],
-      ref: "origin/feature/hfolk_add-emailed-already-icon",
+      ref: "origin/stage",
       path: "/var/www/stage-artist-dashboard",
       repo: "https://github.com/btran-teefury/artist-dashboard-2.git",
       "post-deploy":
